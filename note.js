9cb842d60635a60d608ff72e22574ea2841a7d53
asadsid56 <asadsid56@gmail.com>
2021-03-02 14:10:11 +0100

    edit btn

--- note.js
+++ note.js
@@ -4,56 +4,54 @@ let addNote = document.getElementById("addNote");
 let addTxt = document.getElementById("addTxt");
 
 // Show or Read All Notes
- function read() {
-     // When someone clicks on this Button do create an object
-     let obj = {
-         title: 'Note',
-         id: 0
-     }
-     // Create Notes
-     addNote.addEventListener("click", function () {
-         // Get the Text-area value 
-         let InputValue = addTxt.value;
-         // Creat a card
-         let text = `
+function read() {
+    // When someone clicks on this Button do create an object
+    let obj = {
+        title: 'Note',
+        id: 0
+    }
+    // Create Notes
+    addNote.addEventListener("click", function () {
+        // Get the Text-area value 
+        let InputValue = addTxt.value;
+        // Creat a card
+        let text = `    
         <div class="card col-lg-3 col-md-4 mx-2 my-2 cards" id=${obj.id}>
             <div class="card-body">
-                <i class="fas fa-edit" onclick="Edit(${obj.id})"></i>
+            <button type="button" class="btn btn-outline-warning float-end"><i class="fas fa-edit" onclick="Update(${obj.id})"></i></button>
                 <h5 class="card-title">${obj.title}</h5>
                 <p class="card-text">${InputValue}</p>
                 <button class="btn btn-outline-danger" id="del${obj.id}" onclick="Delete(${obj.id})">Delete Note</button>
             </div>
         </div>
     `
-         // To Display Notes in Div with ID #Note
-         let note = document.querySelector("#Note");
-         note.insertAdjacentHTML('beforeend', text);
-         addTxt.value = "";
-         // console.log(note);
-         obj.id++;
-         console.log(obj);
-     });
- }
- read();
+        // To Display Notes in Div with ID #Note
+        let note = document.querySelector("#Note");
+        note.insertAdjacentHTML('beforeend', text);
+        addTxt.value = "";
+        // console.log(note);
+        obj.id++;
+        // console.log(obj);
+    });
+}
+read();
 
- // Delete Notes
+// Delete Notes
 
- function Delete(id) {
-     document.getElementById(id).remove();
+function Delete(id) {
+    document.getElementById(id).remove();
     //  console.log(`"i am deleting"${id}`);
- }
-
- // Edit Notes 
-
- function Edit(id) {
-    let card = document.getElementById(id);
-    console.log(`"i am editing"${id}`);
-    console.log(card);    
 }
 
+// Edit Notes 
+
+function Update(id) {
+    // let card = document.getElementById(id);
+    console.log(`"i am editing"${id}`);
+    // console.log(card);
+}
+
+
+
 // Search
-
-
-
-
 
