fc21d9412cda9b1fe67d9553b60970ff7e293841
asadsid56 <asadsid56@gmail.com>
2021-03-02 11:28:14 +0100

    changes to nav

--- note.js
+++ note.js
@@ -1,7 +1,10 @@
- // Show or Read All Notes
+// Selectors
+
+let addNote = document.getElementById("addNote");
+let addTxt = document.getElementById("addTxt");
+
+// Show or Read All Notes
  function read() {
-     // Get the Button
-     let addNote = document.getElementById("addNote");
      // When someone clicks on this Button do create an object
      let obj = {
          title: 'Note',
@@ -9,15 +12,15 @@
      }
      // Create Notes
      addNote.addEventListener("click", function () {
-         // Get the Text-area
-         let addTxt = document.getElementById("addTxt");
+         // Get the Text-area value 
+         let InputValue = addTxt.value;
          // Creat a card
          let text = `
         <div class="card col-lg-3 col-md-4 mx-2 my-2 cards" id=${obj.id}>
             <div class="card-body">
                 <i class="fas fa-edit" onclick="Edit(${obj.id})"></i>
                 <h5 class="card-title">${obj.title}</h5>
-                <p class="card-text">${addTxt.value}</p>
+                <p class="card-text">${InputValue}</p>
                 <button class="btn btn-outline-danger" id="del${obj.id}" onclick="Delete(${obj.id})">Delete Note</button>
             </div>
         </div>
@@ -37,14 +40,20 @@
 
  function Delete(id) {
      document.getElementById(id).remove();
-     console.log(`"i am deleting"${id}`);
+    //  console.log(`"i am deleting"${id}`);
  }
 
  // Edit Notes 
 
  function Edit(id) {
-    // let card = document.getElementById(id);
-    // console.log(`"i am editing"${id}`);
-    // console.log(card);
-    
-   }
+    let card = document.getElementById(id);
+    console.log(`"i am editing"${id}`);
+    console.log(card);    
+}
+
+// Search
+
+
+
+
+
