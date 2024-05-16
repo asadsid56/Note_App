4ba8603810179d748220d0abc9d44d89ae4bb0e8
Asad Siddiqui <49947509+asadsid56@users.noreply.github.com>
2024-05-16 11:45:29 +0200

    Add files via upload

--- note.js
+++ note.js
@@ -2,56 +2,84 @@
 
 let addNote = document.getElementById("addNote");
 let addTxt = document.getElementById("addTxt");
+let note = document.querySelector("#Note");
 
 // Show or Read All Notes
-function read() {
+function create() {
     // When someone clicks on this Button do create an object
     let obj = {
-        title: 'Note',
-        id: 0
+        title: "Note",
+        id: 1 
     }
     // Create Notes
     addNote.addEventListener("click", function () {
+
+       
         // Get the Text-area value 
         let InputValue = addTxt.value;
-        // Creat a card
-        let text = `    
-        <div class="card col-lg-3 col-md-4 mx-2 my-2 cards" id=${obj.id}>
-            <div class="card-body">
-            <button type="button" class="btn btn-outline-warning float-end"><i class="fas fa-edit" onclick="Update(${obj.id})"></i></button>
-                <h5 class="card-title">${obj.title}</h5>
-                <p class="card-text">${InputValue}</p>
-                <button class="btn btn-outline-danger" id="del${obj.id}" onclick="Delete(${obj.id})">Delete Note</button>
+
+        if (InputValue === '') {
+            alert("Please write something");
+        } else {
+
+            let text = `    
+            <div class="card col-lg-3 col-md-4 mx-2 my-2 cards" id=${obj.id}>
+                <div class="card-body">
+                <button type="button" class="btn btn-outline-warning d-none float-end"><i class="fas fa-edit" onclick="Update(${obj.id})"></i></button>
+                    <h5 class="card-title">${obj.title}</h5>
+                    <p class="card-text">${InputValue}</p>
+                    <button class="btn btn-outline-danger" id="del${obj.id}" onclick="Delete(${obj.id})">Delete Note</button>
+                </div>
             </div>
-        </div>
-    `
-        // To Display Notes in Div with ID #Note
-        let note = document.querySelector("#Note");
-        note.insertAdjacentHTML('beforeend', text);
+        `
+            // To Display Notes in Div with ID #Note
+            note.insertAdjacentHTML('beforeend', text);
+
+            obj.id++;
+            // console.log(obj);
+        }
+
+    
+        // Creat a card
         addTxt.value = "";
         // console.log(note);
-        obj.id++;
-        // console.log(obj);
+    save(); 
+      
     });
 }
-read();
+create();
 
 // Delete Notes
 
 function Delete(id) {
     document.getElementById(id).remove();
+    save(); 
     //  console.log(`"i am deleting"${id}`);
 }
 
 // Edit Notes 
 
+/*
 function Update(id) {
-    // let card = document.getElementById(id);
+    let card = document.getElementById(id);
+    console.log(card);
     console.log(`"i am editing"${id}`);
-    // console.log(card);
+    if (card.className = "card-text") {
+       let edit = card.tagName("P").value; 
+    }
 }
 
+*/
 
+// Locale Storage
 
-// Search
+function save() {
+    localStorage.setItem("data", note.innerHTML); 
+}
+
+function showData() {
+    note.innerHTML = localStorage.getItem("data"); 
+}
+
+showData(); 
 
